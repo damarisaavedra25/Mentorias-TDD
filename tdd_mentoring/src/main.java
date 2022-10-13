a3e31672c8c497d513883f8bbdd597d8d49c9ca0
damaris0522 <damaris.saavedra@dexcom.com>
2022-10-13 13:34:48 -0700

    Ejercicio 1, Version original

--- tdd_mentoring/src/main.java
+++ tdd_mentoring/src/main.java
@@ -1,8 +1,6 @@
-import java.util.Scanner;
 
 //TDD - Test Driven Development
 class Calculadora {
-    int numero1, numero2;
 
     public int suma(int x, int y) {
         return x + y;
@@ -19,24 +17,4 @@ class Calculadora {
             throw new Exception("Numero no puede ser dividio entre cero.");
         }
     }
-
-    public int multiplicacion(int x, int y) {
-        return x * y;
-    }
-    public int porcentaje(int x, int y) { return x / y *100;}
 }
-//    public int multiplicacion(int x, int y) {
-//        return x * y;
-//    }
-//    public void capturaNumeros(){
-//        Scanner myInput= new Scanner(System.in);
-//        System.out.println("Inserta el primer numero");
-//        numero1 = myInput.nextInt();
-//        System.out.println("Inserta el segundo numero");
-//        numero2 = myInput.nextInt();
-//
-//        System.out.print(" Si se suma un número mayor que cero y un número menor que cero: se restan y se deja el signo del número con mayor valor absoluto");
-//    }
-//
-//    public void ejeutaOperaction(String s, int i) {
-//    }
--- tdd_mentoring/test/MainTest.java
+++ tdd_mentoring/test/MainTest.java
@@ -1,81 +1,39 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Scanner;
-
 public class MainTest {
 
     @Test
-    public void testSuma() {
+    public void testSuma(){
         Calculadora miCalculadora = new Calculadora();
-        int resultado = miCalculadora.suma(10, 10);
-        Assert.assertEquals(20, resultado);
+        int resultado = miCalculadora.suma(10,10);
+        Assert.assertEquals( 20, resultado);
     }
 
     @Test
-    public void testResta() {
+    public void testResta(){
         Calculadora miCalculadora = new Calculadora();
-        int resultado = miCalculadora.resta(10, 10);
-        Assert.assertEquals(0, resultado);
+        int resultado = miCalculadora.resta(10,10);
+        Assert.assertEquals( 0, resultado);
     }
 
     @Test
-    public void testDivisionEntreCero() {
+    public void testDivisionEntreCero(){
         Calculadora miCalculadora = new Calculadora();
         try {
-            miCalculadora.division(100, 0);
+            miCalculadora.division(100,0);
         } catch (Exception e) {
-            Assert.assertEquals("Numero no puede ser dividio entre cero.", e.getMessage());
+            Assert.assertEquals("Not found 404", e.getMessage());
             Assert.assertEquals(Exception.class, e.getClass());
-
         }
     }
 
-    @Test
-    public void testMultiplicacion() {
-        Calculadora miCalculadora = new Calculadora();
-        int resultado = miCalculadora.multiplicacion(10, 10);
-        Assert.assertEquals(100, resultado);
-    }
-
-    @Test
-    public void divisionNormal() throws Exception {
+    public void testOperaction(){
 
         Calculadora miCalculadora = new Calculadora();
-        int resultado = miCalculadora.division(200, 1);
-        Assert.assertEquals(200, resultado);
-    }
+        miCalculadora.insertaPrimerNumero(1);
+        miCalculadora.ejeutaOperaction("-", 2);
 
-    @Test
-    public void testPorcentaje() {
-
-        Calculadora miCalculadora = new Calculadora();
-        int resultado = miCalculadora.porcentaje(10,1);
-        Assert.assertNotEquals(1000,resultado);
+        Assert.assertEquals(1,2);
     }
 }
-//    }
-//    @Test
-//  public void testOperaction(){
-//
-//        Calculadora miCalculadora = new Calculadora();
-////        Scanner myInput= new Scanner(System.in);
-//
-////        System.out.println("Inserta el primer numero");
-////        int numero1 = myInput.nextInt();
-////        miCalculadora.insertaPrimerNumero(numero1);
-////        System.out.println("Inserta el segundo numero");
-////        int numero2 = myInput.nextInt();
-////
-////        System.out.print();
-//
-//
-//
-//        miCalculadora.capturaNumeros();
-//        Assert.assertNotNull(miCalculadora.numero1);
-//        Assert.assertNotNull(miCalculadora.numero2);
-////        miCalculadora.ejeutaOperaction("-", 2);
-////
-//        Assert.assertEquals(-1,2);
-//    }
-//}
